72745a8ba1fad63b6aec87b8aca1548e08312932
Ezequiel Jadib <ezequiel.jadib@southworks.net>
2016-06-14 09:02:03 -0300

    Clean up unused Context constants

--- AuthBot/ContextConstants.cs
+++ AuthBot/ContextConstants.cs
@@ -5,9 +5,6 @@ namespace AuthBot
     {
         public const string PersistedCookieKey = "persistedCookie";
         public const string AuthResultKey = "authResult";
-        public const string OriginalMessageKey = "originalMessage";
-        public const string CurrentMessageFromKey = "messageFrom";
-        public const string CurrentMessageToKey = "messageTo";
         public const string MagicNumberKey = "authMagicNumber";
         public const string MagicNumberValidated = "authMagicNumberValidated";
     }
